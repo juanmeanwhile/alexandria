c73b06db067cb4c7a78acf6fb7629e16911d6545
juanmeanwhile <juanmeanwhile@gmail.com>
2015-10-23 15:19:42 +0200

    Initial commit

---
+++ app/src/main/java/it/jaschke/alexandria/data/Book.java
@@ -0,0 +1,46 @@
+package it.jaschke.alexandria.data;
+
+/**
+ * Created by mengujua on 9/7/15.
+ */
+public class Book {
+
+    private String title;
+    private String subTitle;
+    private String authors;
+    private String imgURL;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+}
